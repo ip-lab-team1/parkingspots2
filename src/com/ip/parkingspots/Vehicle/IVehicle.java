f8d6830354d8d4501e378536cd81b7086725b2db
bogdan2442 <rusubogdan77@gmail.com>
2017-03-27 19:20:03 +0300

    Person

---
+++ src/com/ip/parkingspots/Vehicle/IVehicle.java
@@ -0,0 +1,9 @@
+package com.ip.parkingspots.Vehicle;
+import com.ip.parkingspots.person.AbstractPerson;
+/**
+ * Created by Rusu on 3/27/2017.
+ */
+public interface IVehicle {
+
+    AbstractPerson getOwner();
+}
